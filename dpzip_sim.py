39dd14a2e9d5d605ff05a7b75ac937c8225909f7
Tao Lu <taovcu@gmail.com>
2024-12-31 10:17:56 +0800

    remove dead code
    Signed-off-by: Tao Lu <taovcu@gmail.com>

--- dpzip_sim.py
+++ dpzip_sim.py
@@ -1,11 +1,9 @@
 import argparse
-import os, sys
-from time import gmtime, strftime
-import timeit
+import os
+import sys
 import time
 import gzip
 import zstd
-import lz4
 import lz4.frame
 import numpy as np
 
@@ -15,119 +13,102 @@ chunk_kb = 0
 def getfilesize(filename):
     try:
         ret = os.path.getsize(filename)
-    except:
-        print("except in getfilesize {}".format(filename))
+    except Exception:
+        print(f"Exception in getfilesize: {filename}")
         sys.exit(1)
     return ret
 
 def chunk_file_in_bytes(filename, chunk_kB):
-    ret = []
-
-    # Open original file in read only mode
     if not os.path.isfile(filename):
-        print("No such file as: \"%s\"" % filename)
-        return
+        print(f"No such file: \"{filename}\"")
+        return []
 
-    filesize=getfilesize(filename)
-    with open(filename,"rb") as fr:
+    filesize = getfilesize(filename)
+    with open(filename, "rb") as fr:
         file_bytes = fr.read()
         if not chunk_kB:
             return [file_bytes]
         splitsize = chunk_kB * 1024
-        n_splits = (filesize + splitsize - 1)//splitsize
-        return [file_bytes[i * splitsize:(i + 1) * splitsize] for i in range(n_splits)] 
-
-def chunk_mem_in_bytes(data_content, chunk_kB):
-    ret = []
-
-    datasize=len(data_content)
-    if not chunk_kB:
-        return [data_content]
-
-    splitsize = chunk_kB * 1024
-    n_splits = (datasize + splitsize - 1)//splitsize
-    return [data_content[i * splitsize:(i + 1) * splitsize] for i in range(n_splits)]
+        return [file_bytes[i * splitsize:(i + 1) * splitsize] for i in range((filesize + splitsize - 1) // splitsize)] 
 
 def zstd2dpzip_size_ratio(s):
     if s <= 380:
         return int(s * 1.33)
     if s <= 633:
-        return int(s * (1.33+1.21) / 2)
+        return int(s * (1.33 + 1.21) / 2)
     if s <= 738:
-        return int(s * (1.21+1.21) / 2)
+        return int(s * (1.21 + 1.21) / 2)
     if s <= 1202:
-        return int(s * (1.21+1.05) / 2)
+        return int(s * (1.21 + 1.05) / 2)
     if s <= 1624:
-        return int(s * (1.05+1.03) / 2)
+        return int(s * (1.05 + 1.03) / 2)
     if s <= 1734:
-        return int(s * (1.03+1.03) / 2)
+        return int(s * (1.03 + 1.03) / 2)
     if s <= 1883:
-        return int(s * (1.00+1.03) / 2)
+        return int(s * (1.00 + 1.03) / 2)
     if s <= 2048:
-        return int(s * (1.02+1.00) / 2)
+        return int(s * (1.02 + 1.00) / 2)
     if s <= 2539:
-        return int(s * (1.04+1.02) / 2)
+        return int(s * (1.04 + 1.02) / 2)
     if s <= 3151:
-        return int(s * (1.00+1.04) / 2)
-
+        return int(s * (1.00 + 1.04) / 2)
     return s
-
 
 def compress_bytes(compressor, direction, cmp_level, byte_content):
     start_t = time.time()
-    end_t = time.time()
 
-    start_t = time.time()
     if compressor == 'gzip':
         if not direction:
-            if cmp_level:
-                ret = gzip.compress(byte_content, compresslevel=cmp_level)
-            else:
-                ret = gzip.compress(byte_content, compresslevel=6)
+            level = cmp_level if cmp_level else 6
+            ret = gzip.compress(byte_content, compresslevel=level)
             compressed_size.append(len(ret))
-
-        if direction == 1:
+        else:
             ret = gzip.decompress(byte_content)
 
-    if compressor in ['zstd', 'dpzip']:
+    elif compressor in ['zstd', 'dpzip']:
         if not direction:
-            if cmp_level:
-                ret = zstd.compress(byte_content, cmp_level, 1)
-            else:
-                ret = zstd.compress(byte_content, 3, 1)
-
+            level = cmp_level if cmp_level else 3
+            ret = zstd.compress(byte_content, level, 1)
             if compressor == 'zstd':
                 compressed_size.append(len(ret))
-            if compressor == 'dpzip':
+            elif compressor == 'dpzip':
                 compressed_size.append(zstd2dpzip_size_ratio(len(ret)))
-
-        if direction == 1:
+        else:
             ret = zstd.decompress(byte_content)
 
-    if compressor == 'lz4':
+    elif compressor == 'lz4':
         if not direction:
             ret = lz4.frame.compress(byte_content)
-        if direction == 1:
+            compressed_size.append(len(ret))
+        else:
             ret = lz4.frame.decompress(byte_content)
-        compressed_size.append(len(ret))
 
-    end_t = time.time()
-    comp_time = end_t - start_t
-
-    return ret, len(ret)/len(byte_content), len(byte_content)/1024/1024/(comp_time), 0
-
+    comp_time = time.time() - start_t
+    compression_ratio = len(ret) / len(byte_content)
+    return ret, compression_ratio, len(byte_content) / (1024 * 1024) / comp_time
 
 def print_compress_metrics(compressed_size_list):
-    print("Total compressed bytes: {}".format(sum(compressed_size_list)))
-    print("Percetile compressed file size:\n\t1,\t10,\t20,\t30,\t40,\t50,\t60,\t70,\t80,\t90,\t99")
-    #print("                               ,\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f}".format(np.percentile(compressed_size_list, 1), np.percentile(compressed_size_list, 10),
-    print("\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f},\t{:.0f}".format(np.percentile(compressed_size_list, 1), np.percentile(compressed_size_list, 10),
-            np.percentile(compressed_size_list, 20), np.percentile(compressed_size_list, 30), np.percentile(compressed_size_list, 40), np.percentile(compressed_size_list, 50),
-            np.percentile(compressed_size_list, 60), np.percentile(compressed_size_list, 70), np.percentile(compressed_size_list, 80), np.percentile(compressed_size_list, 90),
-            #np.percentile(compressed_size_list, 95), np.percentile(compressed_size_list, 99), 
-            np.percentile(compressed_size_list, 99)
-        ))
-    print("Average compression ratio: {:.2f}".format(len(compressed_size_list)*chunk_kb*1024 / sum(compressed_size_list)))
+    print(f"Total compressed bytes: {sum(compressed_size_list)}")
+    percentiles = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99]
+    percentile_values = ",\t".join([f"{np.percentile(compressed_size_list, p):.0f}" for p in percentiles])
+    print(f"Percentile compressed file size:\n\t{percentile_values}")
+    average_ratio = (len(compressed_size_list) * chunk_kb * 1024) / sum(compressed_size_list)
+    print(f"Average compression ratio: {average_ratio:.2f}")
+
+def compress_in_mem_chunks(compressor, direction, cmp_level, f_name, chunk_kB):
+    if getfilesize(f_name) <= 1024 * 1024 * 1024:
+        byte_chunk_list = chunk_file_in_bytes(f_name, chunk_kB)
+        for chunk in byte_chunk_list:
+            _, _, _ = compress_bytes(compressor, direction, cmp_level, chunk)
+    else:
+        with open(f_name, 'rb') as f:
+            for piece in read_in_chunks(f):
+                byte_chunk_list = chunk_file_in_bytes(piece, chunk_kB)
+                for chunk in byte_chunk_list:
+                    _, _, _ = compress_bytes(compressor, direction, cmp_level, chunk)
+
+    if not direction:
+        print_compress_metrics(compressed_size)
 
 def read_in_chunks(file_object, chunk_size=1024*1024*1024):
     """Lazy function (generator) to read a file piece by piece.
@@ -138,53 +119,20 @@ def read_in_chunks(file_object, chunk_size=1024*1024*1024):
             break
         yield data
 
-def compress_in_mem_chunks(compressor, direction, cmp_level, f_name, chunk_kB, hw_granularity, m_distance):
-    chunk_cmp_ratio_list = []
-    chunk_cmp_time_list = []
-    compressed_chunk_size_list = []
-    input_num_bytes = 0
-    output_num_bytes = 0
-    wasted_num_bytes = 0
-
-    # if f_name is smaller than 1GB
-    if getfilesize(f_name) <= 1024 * 1024 * 1024:
-        byte_chunk_list = chunk_file_in_bytes(f_name, chunk_kB)
-        for f in byte_chunk_list:
-            output, ratio, speed, ent = compress_bytes(compressor, direction, cmp_level, f)
-            input_num_bytes += len(f)
-            output_num_bytes += len(output)
-            if hw_granularity and len(output) % hw_granularity:
-                wasted_num_bytes += (hw_granularity - len(output) % hw_granularity)
-            chunk_cmp_time_list.append(speed)
-            chunk_cmp_ratio_list.append(ratio)
-            compressed_chunk_size_list.append(len(output))
-    else:
-        with open(f_name, 'rb') as f:
-            for piece in read_in_chunks(f):
-                byte_chunk_list = chunk_mem_in_bytes(piece, chunk_kB)
-                for f in byte_chunk_list:
-                    output, ratio, speed, ent = compress_bytes(compressor, direction, cmp_level, f)
-                    chunk_cmp_time_list.append(speed)
-                    chunk_cmp_ratio_list.append(ratio)
-
-    # compress
-    if not direction:
-        print_compress_metrics(compressed_size)
-
 def do_compress():
     global chunk_kb
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Compress files using various compressors.")
     parser.add_argument("compressor", type=str, choices=['gzip', 'zstd', 'lz4', 'dpzip'],
-                        help="choose the compressor")
+                        help="Choose the compressor: gzip, zstd, lz4, dpzip")
     parser.add_argument("input_file_path", type=str,
-                        help="the input file path must be specified")
+                        help="The input file path must be specified")
     args = parser.parse_args()
-    answer = args.input_file_path
     chunk_kb = 4
 
-    print("Original dataset bytes: {}".format(getfilesize(args.input_file_path)))
-    compress_in_mem_chunks(args.compressor, 0, 1, args.input_file_path, 4, 0, 0)
-
+    input_size = getfilesize(args.input_file_path)
+    print(f"Original dataset bytes: {input_size}")
+    compress_in_mem_chunks(args.compressor, direction=0, cmp_level=1, 
+                           f_name=args.input_file_path, chunk_kB=chunk_kb)
 
 if __name__ == '__main__':
     do_compress()
